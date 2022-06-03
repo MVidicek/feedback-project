7c9968f04db7375efdd4771dc0e8f8e0fa9f47ff
MVidicek <mvidicek@protonmail.com>
2022-06-03 03:26:00 +0200

    display feedbacks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
+
 function App() {
-  return <h1>Hello from the app component</h1>;
+  const [feedback, setFeedbacks] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete this feedback?')) {
+      setFeedbacks(feedback.filter((feedback) => feedback.id !== id));
+    }
+  };
+
+  return (
+    <>
+      <Header></Header>
+      <div className="container">
+        <FeedbackStats feedbacks={feedback}></FeedbackStats>
+        <FeedbackList
+          feedbacks={feedback}
+          handleDelete={deleteFeedback}
+        ></FeedbackList>
+      </div>
+    </>
+  );
 }
 
 export default App;
