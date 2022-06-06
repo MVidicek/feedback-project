adde5523970af2cf29241a359b4f49b1f2dd2081
MVidicek <mvidicek@protonmail.com>
2022-06-06 23:44:30 +0200

    backend json-server

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,20 +1,15 @@
-import { useState, useContext, useEffect } from 'react';
-import FeedbackContext from '../context/FeedbackContext';
-
-function RatingSelect({ selected }) {
-  const [selected, setRating] = useState(10);
-
-  const { feedbackEdit } = useContext(FeedbackContext);
-
-  useEffect(() => {
-    setRating(feedbackEdit.item.rating);
-  }, [feedbackEdit]);
+function RatingSelect({ select, selected }) {
+  // NOTE: We don't need local state here as it's a duplicate of parent state
+  // also no real need for useEffect or context
+  // useEffect(() => {
+  //   select(feedbackEdit.item.rating)
+  // }, [feedbackEdit])
 
   const handleChange = (e) => {
-    setRating(+e.target.value);
-    selected(+e.target.value);
+    select(+e.currentTarget.value);
   };
 
+  // NOTE: simplified with iteration
   return (
     <ul className="rating">
       {Array.from({ length: 10 }, (_, i) => (
