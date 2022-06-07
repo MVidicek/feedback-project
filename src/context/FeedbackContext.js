642806de41e317d07d64f8bd5847db9278abb337
MVidicek <mvidicek@protonmail.com>
2022-06-07 23:18:23 +0200

    add feedback and proxy

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,12 +1,10 @@
 import { createContext, useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [feedback, setFeedback] = useState([]);
-
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
@@ -18,16 +16,14 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(
-      'http://localhost:3000/feedback?_sort=id&_order=desc'
-    );
+    const response = await fetch('/feedback?_sort=id&_order=desc');
     const data = await response.json();
 
     setFeedback(data);
     setIsLoading(false);
   };
 
-  // Add feddback
+  // Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete this feedback?')) {
       setFeedback(feedback.filter((feedback) => feedback.id !== id));
@@ -51,10 +47,19 @@ export const FeedbackProvider = ({ children }) => {
     );
   };
 
-  // Delete feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  // Add feedback
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   return (
